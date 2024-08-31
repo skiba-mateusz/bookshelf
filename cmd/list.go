56c105686c69e4497423dbd2fb1833430b3658c8
Mateusz Skiba <mateusz.skiba114@gmail.com>
2024-08-31 08:43:10 +0200

    refactor: add utility function to display books

--- cmd/list.go
+++ cmd/list.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"strings"
-
 	"github.com/skiba-mateusz/bookshelf/store"
 	"github.com/spf13/cobra"
 )
@@ -12,21 +10,7 @@ func ListBooksCommand(bookStore *store.BookStore) *cobra.Command {
 		Use: "list",
 		Short: "Displays collection of your books",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if len(bookStore.Books()) == 0{
-				cmd.Println("Book collection is empty")
-				return nil
-			}
-			
-			cmd.Println("Your book collection:")
-			cmd.Printf("%-4s %-30s %-30s %-6s %-6s\n", "#", "Title", "Author", "Year", "Read")
-			cmd.Println(strings.Repeat("-", 78))
-			for _, book := range bookStore.Books() {
-				isRead := "[✘]"
-				if book.Read {
-					isRead = "[✔]"
-				}
-				cmd.Printf("%-4d %-30s %-30s %-6d %-6s\n", book.ID, book.Title, book.Author, book.Year, isRead)
-			}
+			printBooks(cmd, bookStore.Books())
 			return nil
 		},
 	}
--- cmd/utils.go
+++ cmd/utils.go
@@ -1,6 +1,30 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"strings"
+
+	"github.com/skiba-mateusz/bookshelf/store"
+	"github.com/spf13/cobra"
+)
+
+// Displays collection of books
+func printBooks(cmd *cobra.Command, books []store.Book) {
+	if len(books) == 0{
+		cmd.Println("Book collection is empty")
+		return
+	}
+	
+	cmd.Println("Your book collection:")
+	cmd.Printf("%-4s %-30s %-30s %-6s %-6s\n", "#", "Title", "Author", "Year", "Read")
+	cmd.Println(strings.Repeat("-", 78))
+	for _, book := range books {
+		isRead := "[✘]"
+		if book.Read {
+			isRead = "[✔]"
+		}
+		cmd.Printf("%-4d %-30s %-30s %-6d %-6s\n", book.ID, book.Title, book.Author, book.Year, isRead)
+	}
+}
 
 // Retrievs flag as string
 func getStringFlag(cmd *cobra.Command, name string) (string, error) {
